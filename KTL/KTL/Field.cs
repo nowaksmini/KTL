03375fbc753c52fbbccae13806a08f6855b42994
siryj <siryj@i.ua>
2016-12-16 13:17:26 +0100

    Field.cs

---
+++ KTL/KTL/Field.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+
+namespace KTL
+{
+    public class Field
+    {
+        private readonly Color EmptyColor = Color.White;
+        public Color Color { get; set; }
+        public bool Enabled { get; set; }
+
+        internal void Select(Color color)
+        {
+            Color = color;
+            Enabled = false;
+        }
+        public void Enable()
+        {
+            Color = EmptyColor;
+            Enabled = true;
+        }
+
+        internal bool IsEmpty()
+        {
+            return Color == EmptyColor;
+        }
+    }
+}
